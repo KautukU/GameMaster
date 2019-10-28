5b0df5abab18c0680170aaeba98ccd64d256d12a
Kautuk <Kautuk.u@gmail.com>
2019-10-28 11:32:11 -0400

    Game Master

---
+++ New Unity Project (1)/Assets/Scripts/Player1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player1 : MonoBehaviour
+{
+
+    // Private variables
+    private Rigidbody2D rBody;
+    public float speed;
+    public int extraJumps;
+
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow) && extraJumps>0)
+        {
+            rBody.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+
+    
+    }
+
+    // Update is called once per frame
+    //FixedUpdate is used when modifying physics values
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        rBody.velocity = new Vector2(inputX * speed, rBody.velocity.y);
+
+
+        //Debug.Log("Horizontal:" + horiz + "Vertical" + vert);
+
+
+
+    }
+}
